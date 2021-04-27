739e7e7f09bd1f38fc39d61a9e249c977ca7bf22
Al Shakib <shakib@alshakib.dev>
2021-04-28 04:31:12 +0600

    Install Sublime Text and Sublime Merge from official repo

--- start.sh
+++ start.sh
@@ -215,6 +215,14 @@ add_repos() {
   yes | flatpak remote-add --if-not-exists flathub "https://flathub.org/repo/flathub.flatpakrepo" &>> "${LOG_FILE}"
   is_failed "Done" "Skipping: FlatHub repo did not install successfully. See log for more info."
 
+  # add sublime text repo
+  print "Importing Sublime HQ gpg key"
+  rpm --import "https://download.sublimetext.com/sublimehq-rpm-pub.gpg" &>> "${LOG_FILE}"
+  is_failed "Done." "Sublime HQ gpg key did not import successfully. See log for more info."
+  print "Installing Sublime HQ repo"
+  dnf config-manager -y --add-repo "https://download.sublimetext.com/rpm/stable/${CPU_ARCH}/sublime-text.repo" &>> "${LOG_FILE}"
+  is_failed "Done" "Skipping: Sublime HQ repo did not install successfully. See log for more info."
+
   # add visual studio code repo
   rpm --import https://packages.microsoft.com/keys/microsoft.asc
   sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
