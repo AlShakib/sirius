becd01204159bfd70ddcdec952fd0c7a2ec954cd
Al Shakib <shakib@alshakib.dev>
2021-04-28 17:53:34 +0600

    Update Internet Speed Meter extension

--- root/usr/share/gnome-shell/extensions/InternetSpeedMeter@alshakib.dev/extension.js
+++ root/usr/share/gnome-shell/extensions/InternetSpeedMeter@alshakib.dev/extension.js
@@ -9,15 +9,19 @@
 const St = imports.gi.St;
 const Main = imports.ui.main;
 const Gio = imports.gi.Gio;
+const GLib  = imports.gi.GLib;
 const Clutter = imports.gi.Clutter;
 const Mainloop = imports.mainloop;
 
 const refreshTime = 1.0; // Set refresh time to one second.
-const unitBase = 1000.0; // 1 GB == 1000MB or 1MB == 1000KB etc.
+const unitBase = 1024.0; // 1 GB == 1024MB or 1MB == 1024KB etc.
+const units = ["KB/s", "MB/s", "GB/s", "TB/s"];
 
 let prevUploadBytes = 0, prevDownloadBytes = 0;
 let uploadSpeed = 0.0, downloadSpeed = 0.0;
 let container, timeout, netSpeed, defaultNetSpeedText;
+let home_dir = GLib.get_home_dir();
+let logSize = 8000; // about 8k
 
 function getNetSpeed() {
   try {
@@ -64,18 +68,35 @@ function getNetSpeed() {
     prevDownloadBytes = downloadBytes;
   } catch(e) {
     netSpeed.set_text( defaultNetSpeedText );
+    saveExceptionLog(e);
   }
   return true;
 }
 
 function netSpeedFormat(speed) {
-  let units = ["KB/s", "MB/s", "GB/s", "TB/s"];
   let i = 0;
   while(speed >= unitBase) {  // Convert speed to KB, MB, GB or TB
-    speed /= unitBase;        // 1MB == 1000KB
+    speed /= unitBase;
     i++;
   }
   return String(speed.toFixed(2) + " " + units[i]);
+}
+
+function saveExceptionLog(e){
+  let log_file = Gio.file_new_for_path( 
+    home_dir + '/.local/var/log/InternetSpeedMeter.log' );
+
+  let log_file_size =  log_file.query_info( 
+    'standard::size', 0, null).get_size();
+  
+  if( log_file_size > logSize ){
+    log_file.replace( null,false, 0, null ).close(null);
+  }
+  e += Date()+':\n' + e;
+  let logOutStream = log_file.append_to( 1, null );
+  logOutStream.write( e, null );
+  logOutStream.close(null);
+
 }
 
 function init() {
